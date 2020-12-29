a16a8cdaa3a8b8941d71ad8f0e3e5eb8b6a297dc
unknown <aamirali812@gmail.com>
2020-12-29 21:40:16 +0530

    update pakage.json

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.post('/child', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(3000 || process.env.PORT, () => {
+    console.log("Server is running on port 3000 " + process.env.PORT)
 })
